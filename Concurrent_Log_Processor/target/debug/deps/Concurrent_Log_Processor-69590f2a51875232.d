ab36971eee6eb9e14d35c7dd20c100170b1b4331
1Boaz <bizbizdrori@gmail.com>
2025-07-18 11:26:23 +0300

    first commit

---
+++ Concurrent_Log_Processor/target/debug/deps/Concurrent_Log_Processor-69590f2a51875232.d
@@ -0,0 +1,5 @@
+/home/boaz/Documents/impresive_projects/Concurrent_Log_Processor/Concurrent_Log_Processor/target/debug/deps/Concurrent_Log_Processor-69590f2a51875232.d: src/main.rs
+
+/home/boaz/Documents/impresive_projects/Concurrent_Log_Processor/Concurrent_Log_Processor/target/debug/deps/libConcurrent_Log_Processor-69590f2a51875232.rmeta: src/main.rs
+
+src/main.rs:
