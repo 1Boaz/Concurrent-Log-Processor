46d52b65e1d6c28899571efbf8ad51e6793bcf40
1Boaz <bizbizdrori@gmail.com>
2025-07-22 17:54:11 +0300

    added some comments and improved preformence first benchmark: 23.92

--- Concurrent_Log_Processor/src/gen_dummy_files.rs
+++ Concurrent_Log_Processor/src/gen_dummy_files.rs
@@ -1,25 +1,48 @@
+use std::io::Write;
+use std::fs::File;
 use enum_derived::Rand;
-use rand::Rng;
+use rand::{Rng};
 use random_string;
 
-#[derive(Rand)]
-enum LogLevel {
+#[derive(Rand, Debug)]
+pub enum LogLevel {
     Error,
     Warning,
     Info,
     Debug,
 }
 
-struct Timestamp {
-    hour: u8,
-    minute: u8,
-    second: u8
+#[derive(Debug)]
+pub struct Timestamp {
+    pub hour: u8,
+    pub minute: u8,
+    pub second: u8
 }
 
-struct Line {
-    timestamp: Timestamp,
-    log_level: LogLevel,
-    message: String
+impl std::fmt::Display for Timestamp {
+    // Formats the `Timestamp` struct for display.
+    //
+    // This implementation of the `fmt` method formats a `Timestamp` by writing
+    // the hour, minute, and second to the provided formatter.
+    //
+    // # Arguments
+    //
+    // * `f` - A mutable reference to a formatter.
+    //
+    // # Returns
+    //
+    // Returns `std::fmt::Result` which indicates whether the operation
+    // succeeded or failed.
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}:{}:{}", self.hour, self.minute ,self.second)
+    }
+}
+
+#[derive(Debug)]
+pub struct Line {
+    pub timestamp: Timestamp,
+    pub log_level: LogLevel,
+    pub message: String
 }
 
 impl Line {
@@ -33,25 +56,59 @@ impl Line {
         random_string::generate_rng(10..20, random_string::charsets::ALPHA)
     }
 
-    fn gen_timestamp() -> Timestamp {
-        let mut rng = rand::rng();
-
+    // Generates a random timestamp.
+    //
+    // The timestamp is in the format of 24 hour clock. The hour is a random
+    // number between 0 and 23, the minute is a random number between 0 and 59,
+    // and the second is a random number between 0 and 59.
+    fn gen_timestamp(rng: &mut rand::rngs::ThreadRng) -> Timestamp {
         Timestamp {
             hour: rng.random_range(0..24),
-            minute: rng.random_range(0..59),
-            second: rng.random_range(0..59),
+            minute: rng.random_range(0..=59),
+            second: rng.random_range(0..=59),
         }
     }
 
-    fn gen_line() -> Line {
+    // Generates a random log line.
+    //
+    // # Examples
+    //23:32:56 Warning ZOcRmBreRrSzUdbC
+    //19:54:58 Debug TGhImvBKfStO
+    pub fn gen_line(rng: &mut rand::rngs::ThreadRng) -> Line {
         Line {
-            timestamp: Line::gen_timestamp(),
+            timestamp: Line::gen_timestamp(rng),
             log_level: Line::gen_log_level(),
             message: Line::gen_message()
-    }
+        }
     }
 }
 
+impl std::fmt::Display for Line {
+    // Formats the `Line` struct for display.
+    //
+    // This implementation of the `fmt` method formats a `Line` by writing
+    // the timestamp, log level, and the message to the provided formatter.
+    //
+    // # Arguments
+    //
+    // * `f` - A mutable reference to a formatter.
+    //
+    // # Returns
+    //
+    // Returns `std::fmt::Result` which indicates whether the operation
+    // succeeded or failed.
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{} {:?} {}", self.timestamp, self.log_level, self.message)
+    }
+}
 
-pub fn generate() {
+pub fn generate() -> std::io::Result<()> {
+    let mut rng = rand::rng();
+    File::create("example.log")?;
+    let mut f = File::options().append(true).open("example.log")?;
+    for _ in 0..1000000 {
+        let line = Line::gen_line(& mut rng);
+        writeln!(&mut f, "{}", line)?;
+    }
+    Ok(())
 }
--- Concurrent_Log_Processor/src/main.rs
+++ Concurrent_Log_Processor/src/main.rs
@@ -1,5 +1,14 @@
 mod gen_dummy_files;
 
 fn main() {
-    println!("Hello, world!");
+    use std::time::Instant;
+    let now = Instant::now();
+
+    // Code block to measure.
+    {
+        gen_dummy_files::generate().unwrap();
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
