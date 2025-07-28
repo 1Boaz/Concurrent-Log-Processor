1e56c0046b162a3e1bc483a7db08bd2d90dda155
1Boaz <bizbizdrori@gmail.com>
2025-07-28 13:21:48 +0300

    Changed the default of loglevel to Error.

--- Concurrent_Log_Processor/src/processing/spreading_tasks.rs
+++ Concurrent_Log_Processor/src/processing/spreading_tasks.rs
@@ -1,6 +1,6 @@
 pub fn process(threads: Option<u128>, file: String, log_level: Option<String>) -> std::io::Result<()> {
     let threads = threads.unwrap_or(20);
-    let loglevel = log_level.unwrap_or("".to_string());
+    let loglevel = log_level.unwrap_or("Error".to_string());
 
     println!("Processing {} with {} threads filtered by {}", file, threads, loglevel);
     Ok(())
